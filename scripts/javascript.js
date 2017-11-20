d77ac5a9d8fcf1c8fd5ac526c9235b2cf459e8dd
burpingdog1 <bdogsproduction@gmail.com>
2017-11-19 20:59:41 -0600

    test push

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,3 +1,28 @@
+/*Project Array Key Vars
+title - project name
+link  - link to github project page
+image - path to image file
+info  - short description of project
+latest- download recent version
+ [title:"", link:"", image:"images/", info:"", latest:""]
+*/
+
+var projects[ 
+	[title:"Soulbound",        
+	 	link:"", 
+	 	image:"images/soulbound",
+		info:"Bind items to players"],
+	[title:"BlockLimiter",
+		link:"",
+		image:"images/blocklimiter",
+		info:"Limit specific blocks players can have exist in the world at one time"],
+	[title:"BetterBlacklisting",
+		link:"",
+		image:"images/betterblacklist",
+		info:"An item blacklisting plugin",
+		latest:""]
+];
+ 
 var slides ["", "", ""];
 int index = 0;
 
@@ -9,4 +34,11 @@ function nextSlide(int pos)
 		index = slides.length-1;
 	else if(index >= slides.length)
 		index = 0;
+}
+
+function displayProjects()
+{
+ 
+		document.write(projects[i].title);
+ 
 }
